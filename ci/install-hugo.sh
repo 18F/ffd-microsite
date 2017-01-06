1caa5cec1bc73294140f8e435d1e562d23c15fcc
John Donmoyer <jdonmoyer@gmail.com>
2017-01-06 12:57:52 -0600

    changing install path

--- ci/install-hugo.sh
+++ ci/install-hugo.sh
@@ -10,4 +10,4 @@ mkdir -p $HOME/bin
 export PATH="$PATH:$HOME/bin"
 wget https://github.com/spf13/hugo/releases/download/v${HUGO_VERSION}/hugo_${HUGO_VERSION}_Linux-64bit.tar.gz
 tar xvfz hugo_${HUGO_VERSION}_Linux-64bit.tar.gz
-cp hugo_${HUGO_VERSION}_Linux-64bit/hugo_${HUGO_VERSION}_Linux-64bit $HOME/bin/hugo
+cp hugo_${HUGO_VERSION}_linux_amd64/hugo_${HUGO_VERSION}_linux_amd64 $HOME/bin/hugo
